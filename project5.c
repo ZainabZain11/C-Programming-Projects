87f935f1e275281a48e0a720269037f260dfa13d
ZainabZain11 <zainabzainnn11@gmail.com>
2026-07-21 14:16:18 +0300

    Compound _ interest_ calculator

---
+++ project5.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <math.h>
+int main(){
+/*  A=p(1+r/n)^nt
+A:amount,P:principal,r:interest rate(decimal),n:number of times(interest is compounded per year,t:time(years))*/
+double principal=0.0;
+double rate=0.0;
+int years=0;
+int times_compounded=0;
+double total=0.0;
+printf("Compound Interest Calculator:\n");
+printf("Enter the principal (p):");
+scanf("%lf",&principal);
+printf("Enter the interest rate % (r):");
+scanf("%lf",&rate);
+rate=rate/100;
+printf("Enter the no.years (t): ");
+scanf("%d",&years);
+printf("Enter the no.times compunded per year (n):");
+scanf("%d",&times_compounded);
+
+total=principal*pow(1+rate/times_compounded,times_compounded*years);
+printf("After %d yesrs , The total will be $ %.2lf ",years,total);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
